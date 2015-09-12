a0e31872fb5fcbf73f7fb2febe31512a89dd73ef
Omri Barak <actualhater@0587356519.wireless.umich.net>
2015-09-12 03:53:32 -0400

    Created Xcode project

---
+++ PencilMeIn/PencilMeIn/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  PencilMeIn
+//
+//  Created by Omri Barak on 9/12/15.
+//  Copyright (c) 2015 PencilMeIn. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
