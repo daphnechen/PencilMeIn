08d785907b7f82f8ab8b557f86603d1a0f0c62e8
Claire Yuan <claireyuan97@gmail.com>
2015-09-12 04:35:19 -0400

    added view controllers

---
+++ PencilMeIn/PencilMeIn/SettingsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SettingsViewController.swift
+//  PencilMeIn
+//
+//  Created by Claire Yuan on 9/12/15.
+//  Copyright (c) 2015 PencilMeIn. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
