88e996d20c531a7e9408154ea888e64a1ede69a9
Claire Yuan <claireyuan97@gmail.com>
2015-09-12 16:21:34 -0400

    added date pickers to business side and splash icon

--- PencilMeIn/PencilMeIn/ScheduleViewController.swift
+++ PencilMeIn/PencilMeIn/ScheduleViewController.swift
@@ -2,25 +2,30 @@
 //  ScheduleViewController.swift
 //  PencilMeIn
 //
-//  Created by Claire Yuan on 9/12/15.
+//  Created by Daphne Chen on 9/12/15.
 //  Copyright (c) 2015 PencilMeIn. All rights reserved.
 //
 
 import UIKit
+import EventKit
 
 class ScheduleViewController: UIViewController {
     
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        // Do any additional setup after loading the view, typically from a nib.
+//    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
     
 }
 
